2e0c845306a80b1246530b1a52eb6a3121efe60a
aditi <aditiarvinds@gmail.com>
2026-05-29 13:10:07 +0530

    takinginput

--- lec_2.java
+++ lec_2.java
@@ -2,7 +2,9 @@
 
  class lec_2 {
     public static void main(String args[]){
-        System.out.println("variables");
+        System.out.print("*\n**\n\n***\n\n\n");
+        
+        }
     }
     
-}
+
