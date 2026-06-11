ae28a155671499ff28acd8bea5788f2e1e8a38f6
aditi <aditiarvinds@gmail.com>
2026-06-11 20:30:57 +0530

    basic function syntax example

---
+++ functions_1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ class functions_1 {
+    public static void printName(String name) {
+        System.out.println(name);
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printName(name); //call to function
+      }
+   
+}
