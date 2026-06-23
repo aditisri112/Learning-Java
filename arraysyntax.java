a253c3077afdbdea547ed1da60d7b6ff340f2478
Aditi Srivastava <aditiarvinds@gmail.com>
2026-06-23 23:42:38 +0530

    array syntax

---
+++ arraysyntax.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class arraysyntax {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0]= 90; //maths
+        marks[1]= 99; //eng
+        marks[2]= 94; //sci
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
--- fibonacci.java
+++ fibonacci.java
@@ -11,9 +11,6 @@ class fibonacci {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a = 0, b = 1;
-
-        System.out.println("Fibonacci Series:");
-
         for (int i = 1; i <= n; i++) {
          System.out.print(a + " ");
 
