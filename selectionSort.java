009e2c36680a77e0733eafedc1ec5c20c9a490be
Aditi Srivastava <aditiarvinds@gmail.com>
2026-07-04 23:48:21 +0530

    selection sort

---
+++ selectionSort.java
@@ -0,0 +1,22 @@
+class selectionSort {
+    public static void main(String[] args) {
+        int arr[] = { 8, 5, 6, 2, 1};
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i]=arr[smallest];
+            arr[smallest] = temp;
+
+        }
+
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+}   
+
