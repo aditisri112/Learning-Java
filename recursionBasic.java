1a09cf96f1c487d5b21ad40a18a404b8a9af99d4
Aditi Srivastava <aditiarvinds@gmail.com>
2026-07-06 18:35:26 +0530

    basic questions of recursion

--- recursionBasic.java
+++ recursionBasic.java
@@ -37,6 +37,47 @@ class recursionBasic {
         prod = prod*z;
         getFactorial(z-1, prod);
     }
+
+    //5.
+    public static void getFibonacciSeries(int f, int g, int p){
+        if(p == 0){
+            return;
+        }
+        int k = f+g;
+        System.out.print(k);
+        getFibonacciSeries(g, k, p-1);
+    }
+
+    //6.
+    public static int getPower(int l, int u){
+        if(u==0){
+            return 1;
+        }
+        if(l==0){
+            return 0;
+        }
+        int powmo = getPower(l , u-1);
+        int pow = l * powmo;
+        return pow;
+    }
+
+    //7.
+     public static int getPow(int e, int r){
+        if(r==0){
+            return 1;
+        }
+        if(e==0){
+            return 0;
+        }
+        if(r%2==0){
+            return getPow(e, r/2)*getPow(e, r/2);
+        }
+        else{
+            return getPow(e, r/2)*getPow(e, r/2)*e;
+        }
+        
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //1.print numbers from 5 to 1
@@ -68,9 +109,34 @@ class recursionBasic {
 
         System.out.println("\n");
 
-        //5.Print the fibonacci sequence till nth term.
-        
-        
+        //5.Print the fibonacci sequence till pth term.
+        System.out.println("enter the number if termms for fibonacci series");
+        int p = sc.nextInt();
+        int f = 0;
+        int g = 1;
+        System.out.println(f);
+        System.out.println(g);
+        getFibonacciSeries(f, g, p-2);
 
+        System.out.println("\n");
+
+        //6.Print l^u (with stack height = n)
+        System.out.println("enter number whose power you want");
+        int l = sc.nextInt();
+        System.out.println("enter the power you want");
+        int u = sc.nextInt();
+        int ans = getPower(l, u);
+        System.out.println(ans);
+
+        System.out.println("\n");
+
+        //7. Print x^n (with stack height = logn)
+        System.out.println("enter number whose power you want");
+        int e = sc.nextInt();
+        System.out.println("enter the power you want");
+        int r = sc.nextInt();
+        int answer = getPow(e, r);
+        System.out.println(answer);
+        
     } 
 }
